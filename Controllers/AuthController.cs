9946d7be40a0f3117c1cd516ce33669ff88ad59e
angelocarlotto@gmail.com <angelocarlotto@gmail.com>
2025-08-29 11:10:56 -0400

    created auth controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi42.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public ActionResult Get(int id) { 
+             return Ok();
+        }
+    }
+}
