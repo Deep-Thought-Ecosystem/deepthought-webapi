03633e03a3b5330fe74f21dc24ce2061780a316e
Angelo Carlotto <angelocarlotto@gmail.com>
2025-09-21 22:30:31 -0400

    fix

--- WebApi42.Bussiness/Services.cs
+++ WebApi42.Bussiness/Services.cs
@@ -4,9 +4,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using WebApi42.DAO;
 using WebApi42.DTO;
 
@@ -59,28 +62,53 @@ namespace WebApi42.Bussiness
             }
             return await CreateRefreshToken(user);
         }
+        static (string? HostBridgeIp, string? HostBridgeMac) GetDockerHostBridgeInfo()
+        {
+            // 1) Get the default gateway IPv4 (host's bridge address from inside the container)
+            string? gatewayIp = NetworkInterface.GetAllNetworkInterfaces()
+                .Where(n => n.OperationalStatus == OperationalStatus.Up &&
+                            n.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+                .SelectMany(n => n.GetIPProperties().GatewayAddresses)
+                .Select(g => g?.Address)
+                .FirstOrDefault(a => a is not null && a.AddressFamily == AddressFamily.InterNetwork &&
+                                     !IPAddress.IsLoopback(a))?
+                .ToString();
 
+            // 2) Best-effort: read the ARP table for that IP to get the MAC
+            string? mac = null;
+            if (!string.IsNullOrWhiteSpace(gatewayIp) && File.Exists("/proc/net/arp"))
+            {
+                // Touch the neighbor so ARP is populated (won't error if it fails)
+                try
+                {
+                    using var s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    s.Blocking = false;
+                    s.ConnectAsync(gatewayIp, 1).Wait(TimeSpan.FromMilliseconds(50));
+                }
+                catch { /* ignore */ }
+
+                foreach (var line in File.ReadLines("/proc/net/arp").Skip(1))
+                {
+                    var parts = Regex.Split(line.Trim(), @"\s+");
+                    if (parts.Length >= 4 && parts[0] == gatewayIp && parts[3] != "00:00:00:00:00:00")
+                    {
+                        mac = parts[3];
+                        break;
+                    }
+                }
+            }
+
+            return (gatewayIp, mac);
+        }
         private async Task<TokenResponseDTO> CreateRefreshToken(User user)
         {
             var toekn = GenerateTocken(user);
             var refreshToekn = await GenerateAndSaveRefreshTokenAsync(user);
             // Get the host name of the machine running the API
-            string localHostIPADddress = "";
-            string hostName = "";
-            try
-            {
-                hostName = Dns.GetHostName();
-                localHostIPADddress = Dns.GetHostEntry(Dns.GetHostName())
-                    .AddressList
-                    .FirstOrDefault(ip => ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)?
-                    .ToString() ?? "";
-            }
-            catch
-            {
-                localHostIPADddress = "";
-            }
+            var (hostBridgeIp, hostBridgeMac) = GetDockerHostBridgeInfo();
 
-            return new TokenResponseDTO { AccessToken = toekn, RefreshToken = refreshToekn, HostIpAddress = localHostIPADddress, HostName= hostName };
+
+            return new TokenResponseDTO { AccessToken = toekn, RefreshToken = refreshToekn, HostIpAddress = hostBridgeIp, HostName= hostBridgeMac };
         }
 
         private string GenerateTocken(User user)
